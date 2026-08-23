28ac47fc59e3795f4af7e1f3e165e982c26dc3d2
parthsawang <parthsawang61@gmail.com>
2026-08-23 22:34:51 +0530

    verify throught jwt token that user is artist or not

--- backend/src/controller/music.controller.js
+++ backend/src/controller/music.controller.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const musicModel = require('../models/music.model');
-
-async function musicUser() {
+const jwt = require("jsonwebtoken")
+async function createMusic(req , res) {
     
-    const {}
+    const {uri, title , artist } = req.body
+     
+
+const token = req.cookies.token;
+
+
+
+if(!token) {
+    return res.status(401).json({
+        message:" unauthorized user",
+    })
+}
+
+try {
+    
+  const decoded= await jwt.verify(token.process.env.JWT_SECRET)
+         
+  if(decoded !== artist) {
+    
+    return res.status(403).json({
+        message: "You dont have acess to create an music"
+    })
+  }
+
+
+
+} catch (error) {
+
+    return res.status(401).json({
+        message: "Unauthorized"
+    })
+}
+
+
 
 }
